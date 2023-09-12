fc8feefd88cc4b1cffded09fd1d6f86a2f7c2631
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-09-11 23:10:00 -0600

    Added basic count logic for num commits

--- src/factories/GithubStatsCommandProcessor.ts
+++ src/factories/GithubStatsCommandProcessor.ts
@@ -1,7 +1,8 @@
 import { WebSocket } from "ws";
 import GithubCommandProps from "../types/GithubCommandProps";
 import CommandProcessor from "./CommandProcessor";
-import axios from "axios";
+import axios, { AxiosHeaders, AxiosResponse } from "axios";
+import GithubMetrics from "../types/GithubMetrics";
 
 class GithubStatsCommandProcessor extends CommandProcessor {
   constructor(private props: GithubCommandProps) {
@@ -12,17 +13,22 @@ class GithubStatsCommandProcessor extends CommandProcessor {
     const { socket }: { socket: WebSocket } = this.props;
     const GH_API_KEY_1: string | undefined = process.env.GH_API_KEY_1;
     const GH_USERNAME_1: string | undefined = process.env.GH_USERNAME_1;
-
-    console.log(`API KEY: ${GH_API_KEY_1}, API USER: ${GH_USERNAME_1}`);
+    const GH_API_KEY_2: string | undefined = process.env.GH_API_KEY_2;
+    const GH_USERNAME_2: string | undefined = process.env.GH_USERNAME_2;
 
     const intervalHandle: NodeJS.Timeout = setInterval(() => {
       if (socket.readyState === WebSocket.OPEN) {
         if (GH_API_KEY_1 && GH_USERNAME_1) {
-          getCommitCountForUser(GH_USERNAME_1, GH_API_KEY_1).then(
-            (totalCommitCount: number) => {
-              socket.send(
-                `Total commits across all repos for [${GH_USERNAME_1}]: ${totalCommitCount}`
-              );
+          getGHUserMetrics(GH_USERNAME_1, GH_API_KEY_1).then(
+            (metrics: GithubMetrics) => {
+              socket.send(JSON.stringify(metrics));
+            }
+          );
+        }
+        if (GH_API_KEY_2 && GH_USERNAME_2) {
+          getGHUserMetrics(GH_USERNAME_2, GH_API_KEY_2).then(
+            (metrics: GithubMetrics) => {
+              socket.send(JSON.stringify(metrics));
             }
           );
         }
@@ -32,43 +38,117 @@ class GithubStatsCommandProcessor extends CommandProcessor {
       }
     }, 30000);
 
-    async function getCommitCountForUser(username: string, token: string) {
+    async function getGHUserMetrics(
+      username: string,
+      token: string
+    ): Promise<GithubMetrics> {
+      const headers: any = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      const metrics: GithubMetrics = new GithubMetrics();
+
       try {
         // Fetch the list of repositories for the user
         const userRepositoriesResponse = await axios.get(
           `https://api.github.com/user/repos`,
           {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
+            headers: headers,
           }
         );
 
         const userRepositories = userRepositoriesResponse.data;
 
-        let totalCommitCount = 0;
-
-        // Iterate through the user's repositories
-        for (const repo of userRepositories) {
-          // Fetch commits for each repository
-          const commitsResponse = await axios.get(`${repo.url}/commits`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-
-          console.log(
-            `Commit count for repo [${repo.url}]: ${commitsResponse.data.length}`
+        metrics.commits = await getCommitsForUser(
+          username,
+          userRepositories,
+          headers
+        );
+        metrics.mergedPRs = await getPRsMergedForUser(
+          username,
+          userRepositories,
+          headers
+        );
+        metrics.linesOfCodeWritten = await getLinesOfCodeWrittenForUser(
+          username,
+          userRepositories,
+          headers
+        );
+        metrics.repositoriesContributed =
+          await getRepositoriesContributedForUser(
+            username,
+            userRepositories,
+            headers
           );
-
-          const commitCount = commitsResponse.data.length;
-          totalCommitCount += commitCount;
-        }
-
-        return totalCommitCount;
       } catch (error) {
-        throw error;
+        console.error(`[Error] Error getting github metrics:  ${error}`);
       }
+
+      return metrics;
+    }
+
+    async function getCommitsForUser(
+      username: string,
+      repositories: any[],
+      authHeaders: AxiosHeaders
+    ): Promise<number> {
+      let totalCommits: number = 0;
+
+      for (const repo of repositories) {
+        let commitsResponse: AxiosResponse<any, any>;
+        let numberOfCommitsOnPage: number = 0;
+        let pageNumber: number = 0;
+
+        do {
+          try {
+            pageNumber++;
+            numberOfCommitsOnPage = 0;
+
+            // TODO, if you save the commit count from last time, theoretically you know the last page you should start the next time you fetch, which will save hella queries.
+            commitsResponse = await axios.get(
+              `${repo.url}/commits?author=${username}&per_page=100&page=${pageNumber}`,
+              {
+                headers: authHeaders,
+              }
+            );
+            numberOfCommitsOnPage = commitsResponse.data.length;
+          } catch (error) {
+            // TODO: I don't love how this behaves on error with the page number logic (what if it errors over and over?), but don't have the time to fix it right now.
+            console.error(
+              `[ERROR] Error getting commit count for page [${repo.url}/commits?author=${username}&per_page=100&page=${pageNumber}]:  ${error}`
+            );
+          }
+        } while (numberOfCommitsOnPage === 100);
+
+        const commitCount = 100 * (pageNumber - 1) + numberOfCommitsOnPage;
+        totalCommits += commitCount;
+      }
+
+      return totalCommits;
+    }
+
+    async function getPRsMergedForUser(
+      username: string,
+      repositories: any[],
+      authHeaders: AxiosHeaders
+    ): Promise<number> {
+      return 0;
+    }
+
+    async function getLinesOfCodeWrittenForUser(
+      username: string,
+      repositories: any[],
+      authHeaders: AxiosHeaders
+    ): Promise<number> {
+      return 0;
+    }
+
+    async function getRepositoriesContributedForUser(
+      username: string,
+      repositories: any[],
+      authHeaders: AxiosHeaders
+    ): Promise<number> {
+      return 0;
     }
   }
 }
