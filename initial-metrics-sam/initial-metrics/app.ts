fae35b958df587afcfbb8c4983b79fef8acbffeb
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-10-08 22:34:59 -0600

    Implemented initial fetch lambda

--- initial-metrics-sam/initial-metrics/app.ts
+++ initial-metrics-sam/initial-metrics/app.ts
@@ -1,17 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { ApiGatewayManagementApi } from 'aws-sdk';
+import Storage from './utils/Storage';
+import GithubMetrics from './types/GithubMetrics';
+import Logger from './utils/Logger';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        console.log(`Hello, I'm the metrics initial response. Connection ID ${event.requestContext.connectionId ?? ""}. You said: ${JSON.stringify(event.body)}`)
+        Logger.appendLog(`Hello, I'm the metrics initial response. Connection ID ${event.requestContext.connectionId ?? ""}. You said: ${JSON.stringify(event.body)}`)
+        try {
+            await Storage.load();
+        } catch (error: any) {
+            Logger.appendError(error);
+            throw new Error('Could not initialize gh-metrics-store connection.');
+        }
+
         const apigatewaymanagementapi = new ApiGatewayManagementApi({ endpoint: 'https://t7kzuciz99.execute-api.us-east-1.amazonaws.com/production', });
+        Logger.appendLog("Api gateway instantiated.");
 
-        console.log("Api gateway instantiated.");
+        const metrics: GithubMetrics = await Storage.get(`metrics`);
 
-        await apigatewaymanagementapi.postToConnection({ ConnectionId: event.requestContext.connectionId ?? "", Data: JSON.stringify({
-            message: 'hello world you saucy bum',
-        })}).promise();
-        console.log("postToConnection awaited.");
+        await apigatewaymanagementapi.postToConnection({ ConnectionId: event.requestContext.connectionId ?? "", Data: JSON.stringify(metrics)}).promise();
+        Logger.appendLog("postToConnection awaited.");
 
         return {
             statusCode: 200,
@@ -19,8 +28,8 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
                 message: 'hello world',
             }),
         };
-    } catch (err) {
-        console.log(err);
+    } catch (err: any) {
+        Logger.appendError(err);
         return {
             statusCode: 500,
             body: JSON.stringify({
